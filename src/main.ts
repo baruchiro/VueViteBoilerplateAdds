2a26aa3d1113118fe8b415605479c9883bd56fcd
Baruch Odem <baruch.odem@checkmarx.com>
2023-01-17 19:08:56 +0200

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
