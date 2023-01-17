cd1af1400545cf8cfa81a47ddd274901dd0dfe53
Baruch Odem <baruch.odem@checkmarx.com>
2023-01-17 18:38:26 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
 /* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
